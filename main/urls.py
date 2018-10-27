2ec84ac42a7e4c9b854211ba41c76b2063811a65
Sophie Tysebaert <sophtys@zaclys.net>
2018-10-27 15:48:39 +0200

    GUI

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,7 @@ from main import views
 urlpatterns = [
     path('', views.home, name="home"),
     path('map/<int:postalCode>', views.map, name="map"),
-    path('farmer_page', views.farmer_page, name="farmer_page")
+    path('farmer_page', views.farmer_page, name="farmer_page"),
+    path('about', views.about, name="about"),
+    path('contact', views.contact, name="contact")
 ]
