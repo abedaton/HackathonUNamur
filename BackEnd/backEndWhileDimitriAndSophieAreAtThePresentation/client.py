ccd90c2c0732a306a8afd2429634814b355c367b
Chronoxx <antoine.bedaton@hotmail.fr>
2018-10-27 15:41:48 +0200

    copy of backend

---
+++ BackEnd/backEndWhileDimitriAndSophieAreAtThePresentation/client.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python3
+
+import socket
+import threading
+import time
+import sys
+import os
+import getpass
+import pickle
+import re
+from signal import signal, SIGPIPE, SIG_DFL
+signal(SIGPIPE, SIG_DFL)
+
+SERVER_IP = "0.0.0.0"
+SERVER_PORT = 10002
+
+class Client:
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+	def __init__(self, address):
+		self.sock.connect((address, SERVER_PORT))
+
+		self.sendInfo()
+
+	def sendInfo(self):
+		answer = -1
+		print("Etes vous:")
+		print("Un Client (1)?")
+		print("Un Vendeur(2)?")
+		answer = input("1/2 ? ")
+		print(answer)
+		while not (answer == "1" or answer == "2"):
+			print("\nVeuillez entrer 1 ou 2")
+			answer = input("1/2 ? ")
+		print("Bienvenue cher %s" % ("client" if answer == "1" else "vendeur"))
+		self.sock.send(b"%s" % b"client" if answer == "1" else b"vendeur")	
+		typeOfUser = "Client" if answer == "1" else "Vendeur"
+		print("Voulez-vous:")
+		print("Se connecter(1)?")
+		print("Creer un compte(2)?")
+		newAnswer = input("1/2 ? ")
+		while not (newAnswer == "1" or newAnswer == "2"):
+			print("\nVeuillez choisir entre 1 ou 2")
+			newAnswer = input("1/2 ? ")
+		self.sock.send(b"%s" % b"connection" if newAnswer == "1" else b"Create")
+		[self.connection(typeOfUser) if newAnswer == "1" else self.createAccount(typeOfUser)]
+
+
+
+
+
+
+	def connection(self, typeOfUser):
+		email = input("\nAddress email: ")
+		while not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+			print("please enter a valid email address")
+			email = input("\nAddress email: ")
+		password = getpass.getpass() # pour ne pas voir le mot de passe s'afficher
+		data = pickle.dumps([typeOfUser, email, password])
+		self.sock.send(data)
+		loggedOrNot = self.sock.recv(1024)
+		print(str(loggedOrNot, "utf-8"))
+
+	def createAccount(self, typeOfUser):
+		email = input("\nAddress email: ")
+		while not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+			print("please enter a valid email address")
+			email = input("\nAddress email: ")
+		password = getpass.getpass()
+		cpassword = getpass.getpass(prompt="Confirm password: ")
+		while password != cpassword:
+			print("Les deux passwords ne match pas")
+			password = getpass.getpass()
+			cpassword = getpass.getpass(prompt="Confirm password: ")
+		name = input("Entrez votre prénom: ")
+		lastname = input("Entrez votre nom: ")
+		while True:
+			phonenumer = input("Entrez votre numero de telephone: ")
+			if phonenumer.isdigit():
+				break
+			else:
+				print("Veuillez entrer un numero de telephone valide!")
+		while True:
+			try:
+				zipCode = int(input("Veuillez entrer votre code postal: "))
+				break
+			except:
+				print("Veuillez entrer un numero de code postal valide!")
+		data = pickle.dumps([typeOfUser, name, lastname, email, password, phonenumer, zipCode])
+		self.sock.send(data)
+		connectedOrNot = self.sock.recv(1024)
+		print(str(connectedOrNot, "utf-8"))
+
+
+if __name__ == "__main__":
+	try:
+		client = Client(SERVER_IP)
+	except Exception as e:
+		print(e)
+		print("Goodbye :)")
---
+++ BackEnd/backEndWhileDimitriAndSophieAreAtThePresentation/server.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python3
+
+import socket
+import threading
+import time
+import sys
+import os
+import sqlite3
+import pickle
+from signal import signal, SIGPIPE, SIG_DFL
+import smtplib 
+import hashlib
+signal(SIGPIPE, SIG_DFL)
+
+SERVER_IP = "0.0.0.0"
+SERVER_PORT = 10002
+
+class Server:
+	sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	connections = [] # Temporary list to stock conections, easier to read than database
+
+	def __init__(self):
+		self.sock.bind((SERVER_IP, SERVER_PORT))
+		self.sock.listen(1)
+		self.goSQL()
+		self.getConnection()
+
+	def goSQL(self):
+		self.conn = sqlite3.connect("../database.db", check_same_thread = False) # Using :memory: pour le moment, eviter de recréer un fichier .db chaque fois
+		self.cursor = self.conn.cursor()
+
+		try:
+			self.cursor.execute("""CREATE TABLE User_Info (
+				name text,
+				lastname text,
+				password text,
+				email text,
+				phonenumber text,
+				zipCode bigint
+				)""")
+			print("Database User_Info créée")
+		except:
+			print("La table User_Info existe déjà")
+			self.cursor.execute("SELECT * FROM User_Info;")
+			print(self.cursor.fetchall())
+		
+		try:
+			self.cursor.execute("""CREATE TABLE Seller_Info (
+				name text,
+				lastname text,
+				password text,
+				email text,
+				phonenumber text,
+				zipCode bigint,
+				stock bigint,
+				price real,
+				openingHours text
+				)""")
+			print("Database Seller_Info créée")
+		except:
+			print("La table Seller_Info existe déjà")
+			self.cursor.execute("SELECT * FROM Seller_Info;")
+			print(self.cursor.fetchall())
+
+
+	def getConnection(self):
+		while True:
+			self.c, a = self.sock.accept()
+			print(str(a[0]) + ":" + str(a[1]) + " vient de se connecter")
+			self.connections.append(self.c)
+			clientThread = threading.Thread(target=self.handleEachUser)
+			clientThread.daemon = True
+			clientThread.start()
+
+
+	def handleEachUser(self):
+		typeOfUser = self.c.recv(1024) 
+		action = self.c.recv(1024)
+		[self.login() if action == b"connection" else self.createAccount()]
+
+	def login(self):
+		compressed_data = self.c.recv(1024)
+		data = pickle.loads(compressed_data)
+		typeOfUser = data[0]
+		email = data[1]
+		password = self.letsHash(data[2])
+		if self.checkOK(typeOfUser, email, password):
+			self.c.send(b"User logged in as " + bytes(email, "utf-8"))
+		else:
+			self.c.send(b"Wrong Username or password!")
+
+	def checkOK(self, typeOfUser, email, password):
+		database = "User_Info" if typeOfUser == "Client" else "Seller_Info"
+		print("check : database = %s, email = %s, password = %s" % (database, email, password))
+		
+		self.cursor.execute("SELECT * FROM " + database + " WHERE email = :email AND password = :password;", {"email": email, "password":password})
+		names = self.cursor.fetchall()
+		print(names)
+		if names == []:
+			return False
+		else:
+			return True
+
+	def createAccount(self):
+		compressed_data = self.c.recv(1024)
+		data = pickle.loads(compressed_data)
+		print(data)
+		typeOfUser = data[0]
+		name = data[1]
+		lastname = data[2]
+		email = data[3]
+		password = self.letsHash(data[4])
+		phonenumber = data[5]
+		zipCode = data[6]
+		self.addUser(typeOfUser, name, lastname, email, password, phonenumber, zipCode)
+
+	def addUser(self, typeOfUser, name, lastname, email, password, phonenumber, zipCode):
+		database = "User_Info" if typeOfUser == "Client" else "Seller_Info"
+		if database == "User_Info":
+			self.cursor.execute("INSERT INTO User_Info(name, lastname, email, password, phonenumber, zipCode) VALUES (?, ?, ?, ?, ?, ?)", (name, lastname, email, password, phonenumber, zipCode))
+			self.conn.commit()
+			print("User added")
+		else:
+			self.cursor.execute("INSERT INTO Seller_Info(name, lastname, email, password, phonenumber, zipCode) VALUES (?, ?, ?, ?, ?, ?)", (name, lastname, email, password, phonenumber, zipCode))
+			self.conn .commit()
+			print("Seller added")
+		self.c.send(b"Vous avez ete inscrit!")
+
+	def letsHash(self, password):
+		for i in range(1000):
+			password = hashlib.md5(bytes(password, "utf-8")).hexdigest()
+		return password
+
+
+if __name__ == "__main__":
+	try:
+		server = Server()
+	except KeyboardInterrupt:
+		print("Goodbye :)")
+
+
