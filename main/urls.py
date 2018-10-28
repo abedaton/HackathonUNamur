2936e840f310538121c79d2ddfdd5769ad59546b
Sophie Tysebaert <sophtys@zaclys.net>
2018-10-28 11:25:39 +0100

    plop

--- main/urls.py
+++ main/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('farmer_page', views.farmer_page, name="farmer_page"),
     path('about', views.about, name="about"),
     path('contact', views.contact, name="contact"),
-    path('login', views.login, name="login")
+    path('login', views.login, name="login"),
+    path('news', views.news, name="news")
 ]
